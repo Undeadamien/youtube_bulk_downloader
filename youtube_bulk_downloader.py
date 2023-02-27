e98c3c36fe49c9d2b910e47631c1e8d840a5723e
Undeadamien <126392901+Undeadamien@users.noreply.github.com>
2023-02-27 11:35:59 +0100

    Update youtube_bulk_downloader.py

--- youtube_bulk_downloader.py
+++ youtube_bulk_downloader.py
@@ -6,14 +6,12 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 
-#the site used to download the videos
 DOWNLOADER = "https://ytmp3.nu/0/youtube-to-mp3"
 
-#folders to check for already downloaded files
-DESTINATION_FOLDERS = ["C:\\Users\\Damien\\Downloads", 
-                       "C:\\Users\\Damien\\Music", 
+# folders to check for already downloaded file
+DESTINATION_FOLDERS = ["C:\\Users\\Damien\\Downloads",
+                       "C:\\Users\\Damien\\Music",
                        "C:\\Users\\Damien\\Desktop\\DRAWING_\\Documentation"]
-
 URL_FOLDERS = [join(dirname(__file__), "urls_to_download\\mp3"),
                join(dirname(__file__), "urls_to_download\\mp4")]
 
@@ -31,36 +29,44 @@ def link_collector(folder):
 
 
 def download(link, format_):
-	
+
     DRIVER = webdriver.Chrome()
     WAIT = WebDriverWait(DRIVER, 10)
     DRIVER.get(DOWNLOADER)
 
-    WAIT.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="url"]')))
+    WAIT.until(EC.visibility_of_element_located
+               ((By.XPATH, '//*[@id="url"]')))
     DRIVER.find_element(By.XPATH, '//*[@id="url"]').send_keys(link)
 
     if format_ == "mp4":
-        WAIT.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="format"]')))
+        WAIT.until(EC.visibility_of_element_located
+                   ((By.XPATH, '//*[@id="format"]')))
         DRIVER.find_element(By.XPATH, '//*[@id="format"]').click()
 
-    WAIT.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="form"]/form/input[3]')))
+    WAIT.until(EC.visibility_of_element_located
+               ((By.XPATH, '//*[@id="form"]/form/input[3]')))
     DRIVER.find_element(By.XPATH, '//*[@id="form"]/form/input[3]').click()
 
-    WAIT.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="download"]/a[1]')))
+    WAIT.until(EC.visibility_of_element_located
+               ((By.XPATH, '//*[@id="download"]/a[1]')))
     DRIVER.find_element(By.XPATH, '//*[@id="download"]/a[1]').click()
 
     def wait_until_file_downloaded():
+
+        # check the downloads folder
         for file_ in listdir(DESTINATION_FOLDERS[0]):
             if "crdownload" in file_.lower():
+
                 sleep(5)
                 wait_until_file_downloaded()
+
     wait_until_file_downloaded()
 
 
 def delete_used_urls():
 
     """
-    Compare the name of each music file with the url of each URL file, 
+    Compare the name of each music file with the url of each URL file,
     if the two match delete the URL file
     """
 
@@ -71,15 +77,18 @@ def delete_used_urls():
         title = title.split()
 
         return title
-    
+
     for url_folder in URL_FOLDERS:
         for folder in DESTINATION_FOLDERS:
 
-            for url_file in [x for x in listdir(url_folder) if x.lower().endswith(".url")]:
-                for music_file in [x for x in listdir(folder) if x.lower().endswith((".mp3", ".mp4"))]:
+            for url_file in [x for x in listdir(url_folder)
+                             if x.lower().endswith(".url")]:
 
-                    if all([True if word.lower() in url_file.lower() 
-                            else False 
+                for music_file in [x for x in listdir(folder)
+                                   if x.lower().endswith((".mp3", ".mp4"))]:
+
+                    if all([True if word.lower() in url_file.lower()
+                            else False
                             for word in simplified_title(music_file)]):
 
                         remove(f"{url_folder}\\{url_file}")
@@ -92,10 +101,13 @@ def main():
     for folder in URL_FOLDERS:
         for link in link_collector(folder):
 
-            if "mp3" in folder: download(link, "mp3")
-            elif "mp4" in folder: download(link, "mp4")
+            if "mp3" in folder:
+                download(link, "mp3")
 
-    delete_used_urls()  #clean up
+            elif "mp4" in folder:
+                download(link, "mp4")
+
+    delete_used_urls()  # clean up
 
 
 if __name__ == "__main__":
